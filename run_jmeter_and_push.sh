41943b817edaa48d0307be3f979f8d51e412ec2e
ismazhar-nisum-com <imazhar@nisum.com>
2025-06-12 21:37:47 +0500

    Update run_jmeter_and_push.sh

--- run_jmeter_and_push.sh
+++ run_jmeter_and_push.sh
@@ -16,24 +16,4 @@ if [ ! -d "$RESULT_DIR/html" ]; then
   exit 1
 fi
 
-# === GitHub CLI + Push Results ===
-rm -rf temp-repo
-gh repo clone $REPO temp-repo
-cd temp-repo
-git checkout -b $BRANCH
 
-mkdir -p results
-cp -r ../$RESULT_DIR/* results/
-
-git add .
-if git diff --cached --quiet; then
-  echo "⚠️ No new changes to commit. Skipping PR creation."
-  exit 0
-fi
-
-git commit -m "Add JMeter test results - $(date)"
-git push origin $BRANCH
-
-gh pr create --base main --head $BRANCH \
-  --title "Add JMeter Test Results - $(date +%F)" \
-  --body "Automated test report upload from local machine"
